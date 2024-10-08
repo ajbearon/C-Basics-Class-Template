5fb851b1f9063b01b4eb0d7f6a58757cb8854f56
ajbearon <99669470+ajbearon@users.noreply.github.com>
2024-10-08 20:23:36 +0100

    Add project files.

---
+++ C# Basics/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+//
+// A console application that asks the user to enter an operator (+-*/),
+// and then asks the user to enter 2 numbers.
+// The application should perform the operation on the 2 numbers and output the result.
+
+Console.Write("Enter an operator (+ - / *): ");
+var key = Console.ReadKey();
+Console.WriteLine();
+
+// TODO - Ask the user to enter 2 numbers
+// Hint: use int.Parse to store the numbers as integers.
+
+// TODO - define a variable to store the result
+switch (key.KeyChar)
+{
+    case '*':
+        Console.WriteLine("multiply");
+        // TODO - implement multiplication
+        break;
+    // TODO - add extra "case" statements for other operators
+    default:
+        Console.Error.WriteLine("Unknown Operator '{0}'", key.KeyChar);
+        break;
+}
+
+
+//
+// TODO - output the calculation and the result e.g. "3 * 8 = 24"
+// Hint: use a format string similar to above
